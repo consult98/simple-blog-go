53ee350cc73d9f47e9053520d79d000c6179e18b
xb <1650768991@qq.com>
2023-06-22 23:49:29 +0800

    未完成的日志组件

---
+++ blog-service/pkg/logger/logger.go
@@ -0,0 +1,104 @@
+package logger //使用lumberjack将日志滚动分级格式化规范输出，未完成和未使用
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"runtime"
+)
+
+type Level int8
+
+type Fields map[string]interface{}
+
+const (
+	LevelDebug Level = iota
+	LevelInfo
+	LevelWarn
+	LevelError
+	LevelFatal
+	LevelPanic
+)
+
+// 应用日志的 Level 和 Fields 的具体类型
+func (l Level) String() string { //将日志分为六个等级，便于在不同场景中记录
+	switch l {
+	case LevelDebug:
+		return "debug"
+	case LevelInfo:
+		return "info"
+	case LevelWarn:
+		return "warn"
+	case LevelError:
+		return "error"
+	case LevelFatal:
+		return "fatal"
+	case LevelPanic:
+		return "panic"
+	}
+	return ""
+}
+
+type Logger struct {
+	newLogger *log.Logger
+	ctx       context.Context
+	fields    Fields
+	callers   []string
+}
+
+func NewLogger(w io.Writer, prefix string, flag int) *Logger {
+	l := log.New(w, prefix, flag)
+	return &Logger{newLogger: l}
+}
+
+func (l *Logger) clone() *Logger {
+	nl := *l
+	return &nl
+}
+
+func (l *Logger) WithFields(f Fields) *Logger { //设置日志等级
+	ll := l.clone()
+	if ll.fields == nil {
+		ll.fields = make(Fields)
+	}
+	for k, v := range f {
+		ll.fields[k] = v
+	}
+	return ll
+}
+
+func (l *Logger) WithContext(ctx context.Context) *Logger { //设置日志上下文属性
+	ll := l.clone()
+	ll.ctx = ctx
+	return ll
+}
+
+func (l *Logger) WithCaller(skip int) *Logger { //设置当前某一层调用栈的信息（程序计数器、文件信息、行号）
+	ll := l.clone()
+	pc, file, line, ok := runtime.Caller(skip)
+	if ok {
+		f := runtime.FuncForPC(pc)
+		ll.callers = []string{fmt.Sprintf("%s: %d %s", file, line, f.Name())}
+	}
+
+	return ll
+}
+
+func (l *Logger) WithCallersFrames() *Logger { //设置当前的整个调用栈信息
+	maxCallerDepth := 25
+	minCallerDepth := 1
+	callers := []string{}
+	pcs := make([]uintptr, maxCallerDepth)
+	depth := runtime.Callers(minCallerDepth, pcs)
+	frames := runtime.CallersFrames(pcs[:depth])
+	for frame, more := frames.Next(); more; frame, more = frames.Next() {
+		callers = append(callers, fmt.Sprintf("%s: %d %s", frame.File, frame.Line, frame.Function))
+		if !more {
+			break
+		}
+	}
+	ll := l.clone()
+	ll.callers = callers
+	return ll
+}
